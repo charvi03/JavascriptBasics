b6704daef82a66123246de85e3f93488ad08edf3
charvi03 <charvikhurana01@gmail.com>
2023-04-18 21:50:47 +0530

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+var myName = "charvi";
+var age = 25;
+console.log(myName);
+//variable names are case sensitive
+//variable name can start with underscrore or special character and $ but cannot start with number or cannot have space in betweeen them.
+// no limit to length of variabble
+//cannot use keywords.
+console.l(age);
